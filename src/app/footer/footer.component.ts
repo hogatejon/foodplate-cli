4c4c9fc92fd0ae17118da12348199f1187aecf6c
Jonathan Hogate <jonathan.hogate@jbhunt.com>
2021-08-23 14:04:58 -0500

    add update needed styling/binding

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  altText: string = 'FoodPlate Logo';
+  appVersionString: string = '1.0.0';
+  appVersionNum: number = this.versionStringToNumber(this.appVersionString);
+  icon: string = 'assets/images/icons/icons-29.png';
+  isCurrent: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.checkForUpdateNeeded();
+  }
+
+  checkForUpdateNeeded() {
+    if (this.appVersionNum <= 100) {
+      this.isCurrent = false;
+    }
+  }
+
+  private versionStringToNumber(version: string) {
+    return parseInt(version.split('.').join(''), 10);
   }
 
 }
